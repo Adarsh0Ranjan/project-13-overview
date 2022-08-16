088ea82a92ab6e03faaacb34da5603b6b8d670f1
adarsh <adarsh0ranjan@gmail.com>
2022-08-16 08:06:01 +0530

    Initial Commit

---
+++ project-13-concepts/project_13_conceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_13_conceptsApp.swift
+//  project-13-concepts
+//
+//  Created by Roro Solutions on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct project_13_conceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
